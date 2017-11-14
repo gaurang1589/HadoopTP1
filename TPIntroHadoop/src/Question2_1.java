9d0a2d33f9d5ecf755ed8b550689be3e31718648
Gaurang <gaurangr@127.0.0.1>
2017-11-14 11:10:06 +0100

    First commit

---
+++ TPIntroHadoop/src/Question2_1.java
@@ -0,0 +1,44 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+
+public class Question2_1 {
+
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+		String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+		String input = otherArgs[0];
+		String output = otherArgs[1];
+		
+		Job job = Job.getInstance(conf, "Question2_1");
+		job.setJarByClass(Question2_1.class);
+		
+		//job.setMapperClass(WordCountMapper.class);
+		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputValueClass(IntWritable.class);
+		
+		//job.setCombinerClass(WordCountReducer.class);
+		//
+		
+		//job.setReducerClass(WordCountReducer.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		
+		FileInputFormat.addInputPath(job, new Path(input));
+		job.setInputFormatClass(TextInputFormat.class);
+		
+		FileOutputFormat.setOutputPath(job, new Path(output));
+		job.setOutputFormatClass(TextOutputFormat.class);
+		
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
+
+	}
+}
