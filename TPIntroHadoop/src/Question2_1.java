a9f84599fe0e30b3910779e68acfc9ab4051cbdc
Gaurang <gaurangr@127.0.0.1>
2017-11-21 11:14:23 +0100

    sceance 2

--- TPIntroHadoop/src/Question2_1.java
+++ TPIntroHadoop/src/Question2_1.java
@@ -1,8 +1,16 @@
+import java.io.IOException;
+import java.net.URLDecoder;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
@@ -11,6 +19,30 @@ import org.apache.hadoop.util.GenericOptionsParser;
 
 
 public class Question2_1 {
+	
+	public static class FlickrMapper extends Mapper<LongWritable, Text, Text, Text> {
+		@Override
+		protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+			IntWritable one =new IntWritable(1);
+			String words []=new String[23];
+			words = value.toString().split("\\t");
+			Country pays= Country.getCountryAt(Double.parseDouble(words[11]),Double.parseDouble(words[10]));
+			if (pays!=null)
+				for(String word: words[8].toString().split(",")){
+					
+					context.write(new Text (pays.toString()),new Text(URLDecoder.decode(word, "UTF-8")));
+				}
+			
+			
+		}
+	}
+
+	public static class MyReducer extends Reducer<Text, Text, Text, IntWritable> {
+		@Override
+		protected void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+				
+		}
+	}
 
 	public static void main(String[] args) throws Exception {
 		Configuration conf = new Configuration();
@@ -21,7 +53,7 @@ public class Question2_1 {
 		Job job = Job.getInstance(conf, "Question2_1");
 		job.setJarByClass(Question2_1.class);
 		
-		//job.setMapperClass(WordCountMapper.class);
+		job.setMapperClass(FlickrMapper.class);
 		job.setMapOutputKeyClass(Text.class);
 		job.setMapOutputValueClass(IntWritable.class);
 		
